80e8b5c5f8819933bd75a513b4e05e7b86fb546e
Manumental32 <manuel.aquino.utn@gmail.com>
2019-04-07 02:54:10 -0300

    Add env.port for Herouku deploy

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var express = require("express");
 var app = express();
-app.listen(3000, () => {
- console.log("Server running on port 3000");
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+ console.log("Server running on port " + port);
 });
 
 app.get("/url", (req, res, next) => {
